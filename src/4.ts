661b25244fd43b76e7be051a7c06e657e32320fe
Denys Troshuk <Troshuk@gmail.com>
2024-02-18 02:15:47 -0400

    hw done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  public comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    this.door = key === this.key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +47,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
